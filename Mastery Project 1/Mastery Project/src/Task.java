b52b038f069f7ea7362f07699697fa89c721c9a5
alwolfeCBE <83241214+alwolfeCBE@users.noreply.github.com>
2024-09-12 08:39:09 -0600

    Add files via upload

---
+++ Mastery Project 1/Mastery Project/src/Task.java
@@ -0,0 +1,18 @@
+public class Task {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String Zellers(int year, int month, int day){
+        String dayOfWeek = "";
+
+
+
+
+        return dayOfWeek;
+
+    }
+
+}
